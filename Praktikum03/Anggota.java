64e978566145f92bae3534db24ace3193f6807ef
nauravlrie <nauravlrie@gmail.com>
2026-09-07 10:17:32 +0700

    tugas2

--- Praktikum03/Anggota.java
+++ Praktikum03/Anggota.java
@@ -38,6 +38,10 @@ public class Anggota {
     }
 
     public void angsur(int nominal) {
-        this.jumlahPinjaman -= nominal;
+        if (nominal < (0.1 * this.jumlahPinjaman)) {
+            System.out.println("Maaf, angsuran harus 10% dari jumlah pinjaman");
+        }else {
+            this.jumlahPinjaman -= nominal;
+        }
     }
 }
--- Praktikum03/TestKoperasi.java
+++ Praktikum03/TestKoperasi.java
@@ -15,12 +15,12 @@ public class TestKoperasi {
         anggota1.pinjam(4000000);
         System.out.println("Jumlah pinjaman saat ini: " + anggota1.getJumlahPinjaman());
 
-        System.out.println("\nMembayar angsuran 1.000.000");
-        anggota1.angsur(1000000);
+        System.out.println("\nMembayar angsuran 200.000");
+        anggota1.angsur(200000); //coba cicil 200.000
         System.out.println("Jumlah pinjaman saat ini: " + anggota1.getJumlahPinjaman());
 
         System.out.println("\nMembayar angsuran 3.000.000");
-        anggota1.angsur(3000000);
+        anggota1.angsur(1000000); //coba cicil 400 ribu
         System.out.println("Jumlah pinjaman saat ini: " + anggota1.getJumlahPinjaman());
     }
     
