3324c533b52281152ef0ecdba5db9043d5c407f3
nauravlrie <nauravlrie@gmail.com>
2026-09-07 09:26:59 +0700

    modifikasi no 4

--- Praktikum03/Motor.java
+++ Praktikum03/Motor.java
@@ -1,10 +1,36 @@
 package Praktikum03;
 
 public class Motor {
-    public String platNomor;
-    public boolean statusMesin;
-    public int kecepatan;
-    
+    private String platNomor;
+    private boolean statusMesin;
+    private int kecepatan;
+
+    public String getPlatNomor() {
+        return platNomor;
+    }
+    public void setPlatNomor(String platNomor) {
+        this.platNomor = platNomor;
+    }
+    public boolean getStatusMesin() {
+        return statusMesin;
+    }
+    public void setStatusMesin(boolean statusMesin) {
+        this.statusMesin = statusMesin;
+    }
+    public int getKecepatan() {
+        return kecepatan;
+    }
+    public void setKecepatan(int kecepatan) {
+        if (!this.statusMesin && kecepatan > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else if (kecepatan > 100) {
+            System.out.println("Kecepatan maksimal adalah 100!");
+            this.kecepatan = 100;
+        } else {
+            this.kecepatan = kecepatan;
+        }
+        
+    }
     public void displayInfo() {
         System.out.println("Plat Nomor: " + this.platNomor);
         System.out.println("Status Mesin: " + (this.statusMesin ? "On" : "Off"));
--- Praktikum03/MotorDemo.java
+++ Praktikum03/MotorDemo.java
@@ -3,19 +3,19 @@ public class MotorDemo {
     public static void main(String[] args) {
         
         Motor motor1 = new Motor();
-        motor1.platNomor ="B 0838 XZ";
-        motor1.kecepatan= 50;
+        motor1.setPlatNomor ("B 0838 XZ");
+        motor1.setKecepatan(50);
         motor1.displayInfo();
 
         Motor motor2 = new Motor();
-        motor2.platNomor ="N 9840 AB";
-        motor2.statusMesin = true;
-        motor2.kecepatan = 40;
+        motor2.setPlatNomor ("N 9840 AB");
+        motor2.setStatusMesin (true);
+        motor2.setKecepatan (120);
         motor2.displayInfo();
 
         Motor motor3 = new Motor();
-        motor3.platNomor = "D 8343 CV";
-        motor3.kecepatan = 60;
+        motor3.setPlatNomor ("D 8343 CV");
+        motor3.setKecepatan (60);
         motor3.displayInfo();
     }
     
