7b4f3e7105a7cdbb9c52d4eea681cf781c51eb07
nauravlrie <nauravlrie@gmail.com>
2026-09-23 19:03:21 +0700

    2 file

--- JobsheetW04/Bunga.java
+++ JobsheetW04/Bunga.java
@@ -1,5 +1,35 @@
 package JobsheetW04;
 
 public class Bunga {
-    
+    private String kodeBunga;
+    private String namaBunga;
+    private double harga;
+
+    public Bunga(String kodeBunga, String namaBunga, double harga) {
+        this.kodeBunga = kodeBunga;
+        this.namaBunga = namaBunga;
+        this.harga = harga;
+    }
+
+    public String getKodeBunga() {
+        return kodeBunga;
+    }
+    public void setKodeBunga(String kodeBunga) {
+        this.kodeBunga = kodeBunga;
+    }
+    public String getNamaBunga() {
+        return namaBunga;
+    }
+    public void setNamaBunga(String namaBunga) {
+        this.namaBunga = namaBunga;
+    }
+    public double getHarga() {
+        return harga;
+    }
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    public String getInfo() {
+        return namaBunga + "(" + kodeBunga + ") @Rp" + String.format("%,.Of", harga);
+    }
 }
--- JobsheetW04/Pelanggan.java
+++ JobsheetW04/Pelanggan.java
@@ -1,5 +1,43 @@
 package JobsheetW04;
 
 public class Pelanggan {
-    
-}
+    private String idPelanggan;
+    private String nama;
+    private String noTelepon;
+    private String alamat;
+
+    public Pelanggan(String idPelanggan, String nama, String noTelepon, String alamat) {
+        this.idPelanggan = idPelanggan;
+        this.nama = nama;
+        this.noTelepon = noTelepon;
+        this.alamat = alamat;    
+    }
+
+    public String getIdPelanggan() {
+        return idPelanggan;
+    }
+    public void setIdPelanggan(String idPelanggan) {
+        this.idPelanggan = idPelanggan;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getNoTelepon(){
+        return noTelepon;
+    }
+    public void setNoTelepon(String noTelepon) {
+        this.noTelepon = noTelepon;
+    }
+    public String getAlamat() {
+        return alamat;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat=alamat;
+    }
+    public String getInfo() {
+        return nama  + idPelanggan + " - Telp: " + noTelepon + ", Alamat: " + alamat;
+    }
+}   
